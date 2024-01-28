9191508a605ed71fc5755f699416820476711396
dippal513 <dip.pal.513@gmail.com>
2024-01-28 22:56:42 +0600

    .env added for production db

--- DB/connectDB.js
+++ DB/connectDB.js
@@ -1,15 +1,13 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-
 dotenv.config();
-
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_URI).then(() => {
+        await mongoose.connect(process.env.PRODUCTION_DB_URL ? process.env.PRODUCTION_DB_URL:process.env.DB_URI).then(() => {
             console.log("connection success to DB..!!")
-        });
+        }); 
     } catch (error) {
         console.log(`connection failed error : ${error}`);
     }
