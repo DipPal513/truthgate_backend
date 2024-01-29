fed740fd8d253c53592d0fb64fc726ff772235b6
dippal513 <dip.pal.513@gmail.com>
2024-01-29 09:50:48 +0600

    cors origin added frontend url

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const port = process.env.PORT || 3000;
         })
 
         // Adjust CORS settings based on your production requirements
-        app.use(cors({ origin: "*", credentials: true }));
+        app.use(cors({ origin: ["http://localhost:3000","https://truthgate-backend.vercel.app"], credentials: true }));
 
         // Routes (Ensure your routes don't need the /api/v1 prefix again)
         app.use("/api/v1", postRouter);
